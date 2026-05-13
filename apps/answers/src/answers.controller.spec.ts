5368f878a8a2ad610f95a4d2a7c103c39d94caad
Nilankur Barik <nilankur.barik.int@infrovate.com>
2026-05-13 12:15:49 +0530

    removed excess service files

--- apps/answers/src/answers.controller.spec.ts
+++ apps/answers/src/answers.controller.spec.ts
@@ -1,17 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnswersController } from './answers.controller';
-import { AnswersService } from './answers.service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
 describe('AnswersController', () => {
   let answersController: AnswersController;
+  let commandBus: CommandBus;
+  let queryBus: QueryBus;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AnswersController],
-      providers: [AnswersService],
+      providers: [
+        {
+          provide: CommandBus,
+          useValue: { execute: jest.fn() },
+        },
+        {
+          provide: QueryBus,
+          useValue: { execute: jest.fn() },
+        },
+      ],
     }).compile();
 
     answersController = app.get<AnswersController>(AnswersController);
+    commandBus = app.get<CommandBus>(CommandBus);
+    queryBus = app.get<QueryBus>(QueryBus);
   });
 
   describe('root', () => {
--- apps/questions/src/questions.controller.spec.ts
+++ apps/questions/src/questions.controller.spec.ts
@@ -1,17 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { QuestionsController } from './questions.controller';
-import { QuestionsService } from './questions.service';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
 describe('QuestionsController', () => {
   let questionsController: QuestionsController;
+  let commandBus: CommandBus;
+  let queryBus: QueryBus;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [QuestionsController],
-      providers: [QuestionsService],
+      providers: [
+        {
+          provide: CommandBus,
+          useValue: { execute: jest.fn() },
+        },
+        {
+          provide: QueryBus,
+          useValue: { execute: jest.fn() },
+        },
+      ],
     }).compile();
 
     questionsController = app.get<QuestionsController>(QuestionsController);
+    commandBus = app.get<CommandBus>(CommandBus);
+    queryBus = app.get<QueryBus>(QueryBus);
   });
 
   describe('root', () => {
